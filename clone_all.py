0ce88acf7f535de0db321ce8c52339028d1289f2
Tiberiu Dascal <tiberiu.purcel@gmail.com>
2026-03-08 10:58:03 +0200

    Refactored on nvim

--- clone_all.py
+++ clone_all.py
@@ -1,21 +1,25 @@
-import os
 import base64
+import os
 
 # The script is expecting an environment variable called GITHUB_TOKEN to be present
 # and its value to be set to your github token
 
-encoded_env = os.getenv('ENCODED_GITHUB_TOKEN')
+encoded_env = os.getenv("ENCODED_GITHUB_TOKEN")
+print(encoded_env)
 decode_bytes = base64.b64decode(encoded_env)
 final_string = decode_bytes.decode("utf-8")
-GITHUB_TOKEN = final_string.replace('\n', '')
+GITHUB_TOKEN = final_string.replace("\n", "")
 
 
 if GITHUB_TOKEN is None:
-    print("You GitHub token is not set!\n Please make sure to configure your environment variable GITHUB_API_TOKEN and the run the program again!")
+    print(
+        "You GitHub token is not set!\n Please make sure to configure your environment variable GITHUB_API_TOKEN and the run the program again!"
+    )
     exit(1)
 
 try:
-    from git import Repo      # GitPython used to clone remote repo to local (GitPython module)
+    # GitPython used to clone remote repo to local (GitPython module)
+    from git import Repo
     from git import exc as exc
 except:
     os.system("pip install GitPython --break-system-packages")
@@ -23,12 +27,13 @@ except:
     from git import exc as exc
 
 try:
-    from github import Github, Auth # used to interact with GitHub API (PyGithub module)
+    # used to interact with GitHub API (PyGithub module)
+    from github import Auth, Github
 except:
     os.system("pip install PyGithub --break-system-packages")
-    from github import Github, Auth
+    from github import Auth, Github
 
-BASE_DIR=f"{os.getenv('HOME')}/REPOS" # change this to match you preference
+BASE_DIR = f"{os.getenv('HOME')}/REPOS"  # change this to match you preference
 
 # login with access token
 auth = Auth.Token(GITHUB_TOKEN)
@@ -51,14 +56,15 @@ success = 0
 for name, repo in repos.items():
     print(f"Cloning repo #+- {name} -+#\n\tFrom: {repo}\n\tTo: {BASE_DIR}/{name}")
     try:
-        Repo.clone_from(f"git@github.com:{user.login}/{name}.git", BASE_DIR+"/"+name);
+        Repo.clone_from(
+            f"git@github.com:{user.login}/{name}.git", BASE_DIR + "/" + name
+        )
         print("\tStatus: ✅\n")
         success += 1
-    except exc.CommandError as e:
+    except exc.CommandError:
         print(f"\tStatus: ⛔️: Repo #+- {name} -+# already exists and is not empty!\n")
         failed += 1
         continue
 
- # Display final statistics
-print(f"Checked {failed+success} repos: SUCCESS:  {success}, FAILED: {failed}")
-
+# Display final statistics
+print(f"Checked {failed + success} repos: SUCCESS:  {success}, FAILED: {failed}")
